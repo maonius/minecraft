c150346df9367ac042bcd98f9fa8148f7aa12362
Maonius <maonius3@gmail.com>
2023-10-07 21:14:30 +0900

    dark theme

--- achivementManager/main.js
+++ achivementManager/main.js
@@ -2,17 +2,42 @@ const THUMBNAIL_W = 84;
 const THUMBNAIL_H = 48;
 let thumbnail_scale = 2;
 
+let achiveImage = document.getElementById('achiveImage');
+let achiveTitle = document.getElementById('achiveTitle');
+let achiveDescription =
+  document.getElementById('achiveDescription');
+let achiveDetail = document.getElementById('achiveDetail');
+let achiveButton = document.getElementById('achiveButton');
+let selectedAchive = null;
+function updateButtonStatus () {
+  if (selectedAchive.unlocked) {
+    achiveButton.innerHTML = "達成済みにする";
+    achiveButton.classList.add('unlocked');
+  } else {
+    achiveButton.innerHTML = "固定する";
+    achiveButton.classList.remove('unlocked');
+  }
+}
+achiveButton.addEventListener('click',
+  function () {
+    selectedAchive.changeStatus();
+    updateButtonStatus();
+  }
+);
+
 let achivementClassNames = [];
 let achivementClass = [];
 let achivements = [];
-let 
 class MinecraftAchivement {
 	constructor(pel, img, dat)
 	{
 		this.unlocked = localStorage.getItem(dat.id) == 'unlocked';
-		this.initElement(img, dat);
+    this.image = img;
+    this.data  = dat;
+		this.initElement(dat);
+    this.initEvent();
 	}
-	initElement(img, dat)
+	initElement(dat)
 	{
 		// Class Element
 		let name = dat.type;
@@ -48,19 +73,59 @@ class MinecraftAchivement {
 		pel.appendChild(elm);
 		elm.appendChild(can);
 		elm.appendChild(che);
-		
-		// draw canvas
+    this.elm = elm;
+		this.drawImageTo(can);
+	}
+  drawImageTo(can)
+  {
+	  // draw canvas
+    let dat = this.data;
 		can.width  = THUMBNAIL_W * thumbnail_scale;
 		can.height = THUMBNAIL_H * thumbnail_scale;
 		let ctx = can.getContext('2d');
-		ctx.drawImage(img,
+		ctx.drawImage(this.image,
 			dat.thumnail.x * THUMBNAIL_W,
 			dat.thumnail.y * THUMBNAIL_H,
 			THUMBNAIL_W, THUMBNAIL_H, 0, 0,
 			can.width, can.height
 		);
-	}
-	
+  }
+  changeStatus()
+  {
+    this.unlocked = !this.unlocked;
+    localStorage.setItem(this.data.id,
+      this.unlocked ? "unlocked" : "locked");
+		if (this.unlocked) {
+      this.elm.classList.add('unlocked');
+    } else {
+      this.elm.classList.remove('unlocked');
+    }
+  }
+  onclick()
+  {
+    selectedAchive = this;
+    this.drawImageTo(achiveImage);
+    achiveTitle.innerHTML = this.data.title;
+    achiveDescription.innerHTML = this.data.description;
+    achiveDetail.innerHTML = this.data.detail;
+    updateButtonStatus();
+  }
+  ondblclick()
+  {
+    this.changeStatus();
+    updateButtonStatus();
+  }
+  // event module
+  initEvent()
+  {
+    let self = this;
+    this.elm.addEventListener('click',
+      () => self.onclick()
+    );
+    this.elm.addEventListener('dblclick',
+      () => self.ondblclick()
+    );
+  }
 }
 
 function initAchivement() {
@@ -77,10 +142,20 @@ function initAchivement() {
 function initElement() {
 	let main = document.getElementById('main');
 	let btn  = document.getElementById('themeButton');
-	btn.addEventListener('click', () => main.classList.toggle('dark') );
+	btn.addEventListener('click',
+    function () {
+      main.classList.toggle('dark');
+      let tf = main.classList.contains('dark')
+      localStorage.setItem('dark', tf ? 'dark' : 'light');
+    }
+  );
+  if (localStorage.getItem('dark') == 'dark') {
+    main.classList.add('dark');
+  }
 }
 
 window.onload = function () {
 	initAchivement();
 	initElement();
+  achivements[0].onclick();
 };
